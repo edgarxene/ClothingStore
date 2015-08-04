0926d3e4a7c05ed467f08159900c7322685db6d1
Edgar Cruz <edgarxene@gmail.com>
2015-08-03 23:00:05 -0500

    Commit inicial

---
+++ resources/views/admin/categories/create.blade.php
@@ -0,0 +1,15 @@
+@extends('app')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading"> Panel de Administracion - Categorias</div>
+                    <div class="panel-body">
+                       
+                    </div> 
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
