c071f7c8dbdda005b0ccb6dba36f1f411302d509
Edgar Cruz <edgarxene@gmail.com>
2015-09-20 11:44:54 -0500

    Revert "Revert "vista""
    This reverts commit 619163df6ec5667882c17075c7f9121aee2d22dd.

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,15 +1,9 @@
 @extends('layoutAdmin')
 @section('content')
-<div class="container">
-        <div class="row">
-            <div class="col-md-6 col-md-offset-4">
-                <div class="panel panel-default">
+            <div class="panel panel-default">
                     <div class="panel-heading">Categories Admin Panel</div>
                     <div class="panel-body">
                         PANEL DE ADMINISTRACION
                     </div> 
-                </div>
-            </div>
-        </div>
-    </div>
+                </div>           
 @endsection
