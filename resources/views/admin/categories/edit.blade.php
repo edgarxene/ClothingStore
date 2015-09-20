88d1d68fa65da2dea27d8a368315f66ae2d956dd
Edgar Cruz <edgarxene@gmail.com>
2015-09-19 23:21:00 -0500

    vista

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -1,14 +1,12 @@
 @extends('layoutAdmin')
 @section('content')
-<div class="container">
-        <div class="row">
-            <div class="col-md-6 col-md-offset-4">
-                <div class="panel panel-default">
-                    <div class="panel-heading"> Panel de Administracion - Categorias</div>
-                    <div class="panel-body">
-                        <div id='admin'>
-                            <p>Aqui tu puedes editar la categoria seleccionada.</p>
-                            <div class="form-group">
+
+    <div class="panel panel-default">
+        <div class="panel-heading"> Panel de Administracion - Categorias</div>
+            <div class="panel-body">
+                <div id='admin'>
+                     <p>Aqui tu puedes editar la categoria seleccionada.</p>
+                     <div class="form-group">
                                 
                         <h2>Editar Categoria</h2><hr/>                 
 
@@ -21,10 +19,7 @@
                         {!! link_to('admin/categories', $title = 'Canelar', array('class'=>'btn btn-danger')) !!}
                         {!! Form::close() !!}
 
-                         
-                        </div> 
-                </div>
-            </div>
-        </div>
-    </div>
+                        
+                     </div> 
+          
 @endsection
